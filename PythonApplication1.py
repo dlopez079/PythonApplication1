3b1f0b401b9aa89289f38ae891df2b2806650a9f
David Lopez <50810168+dlopez079@users.noreply.github.com>
2022-01-25 00:13:27 -0500

    Installed Python Support, Imported sys and used functions in both the REPL and CLI, and used the debugger to check code.

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,5 +1,15 @@
-import sys 
 from math import cos, radians
 
-for i in range(360):
-    print(cos(radians(i)))
+# Create a string with spaces proportional to a cosine of x in degrees
+def make_dot_string(x):
+    rad = radians(x)                             # cos works with radians
+    numspaces = int(20 * cos(rad) + 20)          # scale to 0-40 spaces
+    st = ' ' * numspaces + 'o'                   # place 'o' after the spaces
+    return st
+
+def main():
+    for i in range(0, 1800, 12):
+        s = make_dot_string(i)
+        print(s)
+
+main()
